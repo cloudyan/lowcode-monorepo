660834a6e8f23f53034ede91a251971331c47ff0
cloudyan <1395093509@qq.com>
2023-01-11 18:51:19 +0800

    chore: rename types dir

--- packages/lowcode-react-renderer/.fatherrc.ts
+++ packages/lowcode-react-renderer/.fatherrc.ts
@@ -1,16 +1,5 @@
 import { defineConfig } from 'father';
 
-//
 export default defineConfig({
-  // esm: { output: 'es' },
-  // cjs: { output: 'lib' },
-  // umd: { output: 'dist' },
-  esm: {  },
-  cjs: {  },
-  umd: {  },
-  // prebundle: {},
+  extends: '../../.fatherrc.base.ts',
 });
-
-
-// 默认值为 dist/esm，如果想保持原目录，需增加 output 配置项
-// output: 'es'
